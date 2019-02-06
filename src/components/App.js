2a417991e9b75b97bbb3ad254ea03b4c1e7382ec
piebalestra <pie.balestra@gmail.com>
2019-02-06 16:48:53 +0100

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Posts from './Posts'
+import PostForm from './PostForm'
+import { Provider } from 'react-redux'
+import store from '../store'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
